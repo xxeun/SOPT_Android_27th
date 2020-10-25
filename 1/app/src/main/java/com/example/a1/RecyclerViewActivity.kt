a965c0c033d824364e59116bf6f539ba39dd4fe2
DESKTOP-KC3CG2R\itsme <itsmege0309@gmail.com>
2020-10-25 16:53:10 +0900

    1차 세미나 과제

---
+++ 1/app/src/main/java/com/example/a1/RecyclerViewActivity.kt
@@ -0,0 +1,33 @@
+package com.example.a1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.GridLayout
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.synthetic.main.activity_home.*
+
+class RecyclerViewActivity : AppCompatActivity() {
+    private lateinit var sampleAdapter: SampleAdapter
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+
+        view_rcv.layoutManager = GridLayoutManager(this, 3, RecyclerView.VERTICAL, false)
+
+        sampleAdapter = SampleAdapter(this)
+
+        view_rcv.adapter = sampleAdapter
+
+        sampleAdapter.data = mutableListOf(
+            SampleData("1","소개", "2020.10.18","남가은, 22살, 숙명여자대학교 소비자경제학과, IT공학전공"),
+            SampleData("2","관심 분야","2020.10.18","IT, 소비자학, 시장분석"),
+            SampleData("3","깃허브","2020.10.18","안드로이드 초보의 깃허브 : https://github.com/xxeun")
+        )
+        sampleAdapter.notifyDataSetChanged()
+
+
+    }
+}
